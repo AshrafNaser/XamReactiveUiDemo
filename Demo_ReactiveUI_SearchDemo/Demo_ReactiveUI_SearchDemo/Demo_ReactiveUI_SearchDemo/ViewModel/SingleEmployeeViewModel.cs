610a063afd27975a3fe6ec77a42a9203abc495c9
Ashraf Naser <a3ssely@hotmail.com>
2018-07-09 08:48:01 -0700

    changes

---
+++ Demo_ReactiveUI_SearchDemo/Demo_ReactiveUI_SearchDemo/Demo_ReactiveUI_SearchDemo/ViewModel/SingleEmployeeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Demo_ReactiveUI_SearchDemo.Model;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_ReactiveUI_SearchDemo.ViewModel
+{
+    public class SingleEmployeeViewModel : BaseViewModel
+    {
+        public SingleEmployeeViewModel()
+        {
+
+        }
+        public SingleEmployeeViewModel(Employee employee)
+        {
+            SelectedEmployee = employee;
+        }
+
+
+        private Employee _selectedEmployee;
+        public Employee SelectedEmployee
+        {
+            get => _selectedEmployee;
+            set => this.RaiseAndSetIfChanged(ref _selectedEmployee, value);
+        }
+    }
+}
---
+++ Demo_ReactiveUI_SearchDemo/Demo_ReactiveUI_SearchDemo/Demo_ReactiveUI_SearchDemo/Views/BaseContentPage.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI.XamForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_ReactiveUI_SearchDemo.Views
+{
+    public class BaseContentPage<TViewModel> : ReactiveContentPage<TViewModel> where TViewModel : class
+    {
+    }
+}
