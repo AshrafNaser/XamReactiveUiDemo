0acf87f8cccb33f8a64c1cefd23b2d80fa89ac5a
Ashraf Naser <a3ssely@hotmail.com>
2018-07-09 08:47:46 -0700

    chnages
    the latest Chnges

---
+++ Demo_ReactiveUI_SearchDemo/Demo_ReactiveUI_SearchDemo/Demo_ReactiveUI_SearchDemo/Views/EmployeesPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Demo_ReactiveUI_SearchDemo.ViewModel;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo_ReactiveUI_SearchDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmployeesPage : BaseContentPage<EmployeeViewModel>
+    {
+        public EmployeesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+           
+        }
+
+    }
+}
